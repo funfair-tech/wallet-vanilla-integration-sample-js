090a96e99b446ad99b195eca6194df2a6b953b07
Josh Stevens <joshstevens19@hotmail.co.uk>
2020-09-09 08:49:53 +0100

    fully working with pure js

---
+++ README.md
@@ -0,0 +1,18 @@
+This project shows how you can integrate the fun wallet with pure javascript (no frameworks).
+
+Full documentation is available on how you can integrate is [here](https://funfair-tech.github.io/fun-wallet-docs/guide/web-sdk/installing-sdk.html)
+
+## Install
+
+```bash
+$ npm install
+$ npm start
+```
+
+This will load the app on http://localhost:3001
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+## Live demo
+
+To see it in action please go to - https://funfair-tech.github.io/wallet-react-integration-sample-js (this is the react codebase which is deployed)
--- src/components/app/index.js
+++ src/components/app/index.js
@@ -6,36 +6,37 @@ export default class App {
     if (!elem) return;
     this.elem = elem;
 
-    isAuthenticated$.subscribe((value) => {
-      if (restoreAuthenticationTaskCompleted$.value) {
-        if (value) {
-          document.getElementById("logged-in-content").style.display = "block";
-          document.getElementById("logged-out-content").style.display = "none";
-
-          document.getElementById(
-            "fun-wallet-follower"
-          ).src = window.funwallet.getWalletFollowerURL();
-        } else {
-          document.getElementById("logged-in-content").style.display = "none";
-          document.getElementById("logged-out-content").style.display = "block";
-        }
-      }
-    });
-
-    restoreAuthenticationTaskCompleted$.subscribe((value) => {
+    isAuthenticated$.pipe(rxjs.operators.skip(1)).subscribe((value) => {
       if (value) {
-        document.getElementById("loading").style.display = "none";
+        document.getElementById("logged-in-content").style.display = "block";
+        document.getElementById("logged-out-content").style.display = "none";
+
+        document.getElementById(
+          "fun-wallet-follower"
+        ).src = window.funwallet.getWalletFollowerURL();
+      } else {
         document.getElementById("logged-in-content").style.display = "none";
         document.getElementById("logged-out-content").style.display = "block";
       }
+      document.getElementById("loading").style.display = "none";
     });
+
+    restoreAuthenticationTaskCompleted$
+      .pipe(rxjs.operators.skip(1))
+      .subscribe((value) => {
+        if (value && !isAuthenticated$.value) {
+          document.getElementById("loading").style.display = "none";
+          document.getElementById("logged-in-content").style.display = "none";
+          document.getElementById("logged-out-content").style.display = "block";
+        }
+      });
   }
 
   render() {
     if (this.elem)
       this.elem.innerHTML = `
         <section data-component="app">
-          <img src="" id"logo" alt="logo" />
+          <img src="./logo.svg" id"logo" alt="logo" />
           <p id="loading">Loading please wait...</p>
           <div id="content">
             <div id="logged-out-content">
@@ -53,7 +54,7 @@ export default class App {
               </div>
               <iframe
                 id="fun-wallet-follower"
-                (load)="followerLoaded()"
+                onload="followerLoaded()"
                 is-fun-wallet="true"
                 is-follower="true"
               ></iframe>
