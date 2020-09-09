28bec4c6094662fd7bfee0218644b7cb68b7a945
Josh Stevens <joshstevens19@hotmail.co.uk>
2020-09-09 08:51:16 +0100

    update entry point for app

--- src/index.js
+++ src/index.js
@@ -7,11 +7,5 @@ import App from "./components/app";
 window.addEventListener("load", () => {
   const app = new App(document.getElementById("app"));
 
-  // A very simple component setup
   app.render();
-
-  // // Render the time every 1s
-  // setInterval(() => {
-  //   app.render()
-  // }, 1000)
 });
